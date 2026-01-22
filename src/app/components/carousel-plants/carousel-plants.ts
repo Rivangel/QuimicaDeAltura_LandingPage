22772407e499c1c53d05fb92bfd92db27c4e27bd
AngelRivera <20233l001142@utcv.edu.mx>
2026-01-22 01:21:05 -0600

    feat(carousel-plants): implement medicinal plants carousel

--- src/app/components/carousel-plants/carousel-plants.ts
+++ src/app/components/carousel-plants/carousel-plants.ts
@@ -7,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrl: './carousel-plants.scss',
 })
 export class CarouselPlants {
+  images: string[] = [
+    'resources/images/aloe_1280x853.jpg',
+    'resources/images/manzanilla_1280x855.jpg',
+    'resources/images/menta_1280x960.jpg'
+  ];
+  currentIndex: number = 0;
+  intervalId: any;
 
+  ngOnInit() {
+    this.startAutoSlide();
+  }
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 4000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  nextSlide() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  goToSlide(index: number) {
+    this.currentIndex = index;
+    this.stopAutoSlide();
+    this.startAutoSlide(); // Restart timer on manual interaction
+  }
 }
