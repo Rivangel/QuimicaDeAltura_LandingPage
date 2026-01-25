eeec4a5f78f70424efd5ad3bd284ab033dc9f926
JOSE MANUEL LARA VILLALOBOS <20233l001152@utcv.edu.mx>
2026-01-24 19:24:34 -0600

    Remove image copy batch scripts
    Deleted copy_images.bat and fix_images.bat scripts used for copying mockup images. These scripts are no longer needed in the project.

--- fix_images.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set SRC=C:\Users\villa\.gemini\antigravity\brain\e37dee6a-4b1e-427a-80c4-13b5cb3f719d
-set DST=c:\Users\villa\Documents\GitHub\QuimicaDeAltura_LandingPage\public\resources\images
-echo Copying...
-copy /Y "%SRC%\uploaded_media_0_1769299220735.png" "%DST%\app_mockup_home.png"
-copy /Y "%SRC%\uploaded_media_1_1769299220735.png" "%DST%\app_mockup_scan.png"
-copy /Y "%SRC%\uploaded_media_2_1769299220735.png" "%DST%\app_mockup_chat.png"
-dir "%DST%\app_mockup_*"
-echo Done.
