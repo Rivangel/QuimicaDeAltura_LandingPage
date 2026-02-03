832d14b5ed219e915884147e60e7c4b225475002
Christba <christba455@gmail.com>
2026-02-03 16:28:22 -0600

    feat(radial-menu): Enhance radial menu functionality and styling with dynamic content updates

--- src/app/components/radial-menu/radial-menu.ts
+++ src/app/components/radial-menu/radial-menu.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radial-menu',
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './radial-menu.scss',
 })
 export class RadialMenu {
+  @Input() activeButton: string | null = null;
+  @Output() contentChange = new EventEmitter<void>();
+  @Output() setOriginalContent = new EventEmitter<void>();
+  @Output() setMission = new EventEmitter<void>();
+  @Output() setVision = new EventEmitter<void>();
+  @Output() setValues = new EventEmitter<void>();
+  @Output() setContact = new EventEmitter<void>();
+  @Output() setBusinessModel = new EventEmitter<void>();
 
+  onShowcaseButtonClick() {
+    this.contentChange.emit();
+  }
+
+  onHeartButtonClick() {
+    this.setOriginalContent.emit();
+  }
+
+  onMissionClick() {
+    this.setMission.emit();
+  }
+
+  onVisionClick() {
+    this.setVision.emit();
+  }
+
+  onValuesClick() {
+    this.setValues.emit();
+  }
+
+  onContactClick() {
+    this.setContact.emit();
+  }
+
+  onBusinessModelClick() {
+    this.setBusinessModel.emit();
+  }
 }
